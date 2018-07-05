ab4e79f084f15b4e47e99bc48f2c993ce1beddd0
alex22presanto <40808614+alex22presanto@users.noreply.github.com>
2018-07-04 22:27:46 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,22 +3,54 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
-	
+{	var num1;
+    var num2;
+    var total;
+    num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    total = num1 + num2;
+    alert("el suma es " + total);
+
 }
 
 function restar()
-{
+{   var num1;
+    var num2;
+    var total;
+    num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    total = num1 - num2;
+    alert("la resta es " + total);
 	
 }
 
 function multiplicar()
-{ 
+{   var num1;
+    var num2;
+    var total;
+    num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    total = num1 * num2;
+    alert("la multiplicación es " + total);
 	
 }
 
 function dividir()
-{
+{   var num1;
+    var num2;
+    var total;
+    num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    total = num1 / num2;
+    alert("la divición es " + total);
 	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -3,7 +3,12 @@ transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
-{
+{   var sueldo;
+    var total;
+    sueldo = parseInt(document.getElementById("sueldo").value);
+    total= sueldo * 10 / 100;
+    sueldo = sueldo + total;
+    document.getElementById("resultado").value = sueldo;
 
 	
 }
