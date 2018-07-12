1b829eba00f90455aa3549f736b94942b0f2d318
alex22presanto <40808614+alex22presanto@users.noreply.github.com>
2018-07-11 21:36:25 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+var numero =Math.floor(Math.random()* (11-1)+1);
 
+if (numero >= 9)
+{
+alert("Exelente");	
+}
+	else if (numero< 4)
+	{
+	alert("Vamos, la proxima se puede");
+	}
+	else
+	{
+	alert("Aprobado");
+	}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,8 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+var numero =Math.floor(Math.random()* (11-1)+1);
+ alert ("Su numero es: " + numero);
+
 
 }//FIN DE LA FUNCIÓN
