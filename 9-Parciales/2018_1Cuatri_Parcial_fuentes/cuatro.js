ef2d6aa362868ac14868d764984d58615d1227cf
alex22presanto <40808614+alex22presanto@users.noreply.github.com>
2018-07-11 22:05:50 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,26 @@
 function mostrar()
 {
-
+var numuno;
+var numdos;
+numuno = parseInt(prompt("ingrese el primer numero: "));
+numdos =  parseInt(prompt("ingrese el segundo numero: "));
+if (numuno == numdos)
+{
+alert("El numero es: " + numuno.toString() + numdos);
+} 
+    else if (numuno > numdos)
+    {
+    alert("La resta es: " + (numuno - numdos));
+    }
+    else
+    {
+        if((numuno + numdos)> 10 )
+        {
+        alert("La suma es "+(numuno + numdos)+ " y es mayor a 10");
+        }
+        else
+        {
+        alert("La suma es: " + (numuno + numdos));        
+        }
+    }
 }
